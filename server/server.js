b2fd0c993c1ae4659b30a1b1808986a69f27ce3b
Gagana Gavesh <gaveshgamage@gmail.com>
2021-06-17 12:46:43 +0530

    heroku configurations added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { O_DIRECTORY } = require('constants');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');//public path eka set karanawa methana indan tyna
 //'..' kiyanne ekak eliyata kiyana eka
+const port = process.env.PORT || 3000;//environment variables wala PORT ekata heroku eka set karanawa port num ekak
 
 app.use(express.static(publicPath));//customise the server ,now it serves up all in the public directory
 
@@ -14,11 +15,17 @@ app.get('*', (req, res) =>{
 });//meka allow karanawa function ekak run karanna, kauru hari server ekata get request ekak kalama
 //'*' to match all unhandled paths
 
-app.listen(3000, () => {
-    console.log('Server is up !');
-});//port we load this, me function eka wada karanawa server eka up una gaman
+// app.listen(3000, () => {//3000 heroku danawa nam hariyanne ne, heroku eka dynamic nissa meka wenas karanna ona
+//     console.log('Server is up !');
+// });//port we load this, me function eka wada karanawa server eka up una gaman
 
+app.listen(port, () => {//heroku eken dana port ekata thama listen karan inne
+    console.log('Server is up !');
+});
 
 // 1) create express applicationCache
 // 2) tell it use public directory
 // 3) hey when some one ask for something give them indexedDB.html
+
+//heroku eka balanawa start kiyala ekak tynawada kiyala package.json ekee
+//ilangata eketa adala command eka thama run karannee
